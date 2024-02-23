ce40412abecbf3f8deac62cefb1243ad92b1af7a
Richard Lin <richard.lin@berkeley.edu>
2024-02-23 14:40:33 -0800

    wip pydantic

--- netlist_compiler.py
+++ netlist_compiler.py
@@ -1,47 +1,48 @@
 from typing import TypedDict, Tuple, Optional, Any
 import json
 import inspect
+from pydantic import BaseModel
 
 import sys
 import os.path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'PolymorphicBlocks'))
 
 
-class JsonNet(TypedDict):
+class JsonNet(BaseModel):
   nodeId: str  # unused, internal node ID
   portIdx: str  # unused
   name: str  # name of node, may be same as node ID
   portName: str  # name of port (consistent w/ HDL)
 
-class JsonNodePort(TypedDict):
+class JsonNodePort(BaseModel):
   name: str  # name of port (consistent w/ HDL)
   leftRightUpDown: str  # ignored
   type: str  # type of port
   array: bool  # whether port is an array
   _idx: int
 
-class JsonNodeArgParam(TypedDict):
+class JsonNodeArgParam(BaseModel):
   name: str  # name of parameter (consistent w/ HDL)
   type: str  # type of parameter, ...
   defaultValue: Any  # value of parameter
 
-class JsonNodeData(TypedDict):
+class JsonNodeData(BaseModel):
   name: str  # ???
   type: str  # node class
   superClasses: list[str]  # superclasses, excluding self type
   ports: list[JsonNodePort]
   argParams: list[JsonNodeArgParam]
 
-class JsonNode(TypedDict):
+class JsonNode(BaseModel):
   data: JsonNodeData
   ports: list[list[str]]  # port names
   id: str  # node ID
 
-class JsonGraph(TypedDict):
+class JsonGraph(BaseModel):
   nodes: list[Tuple[str, JsonNode]]
   edges: list[Tuple[str, Any]]  # ignored
 
-class JsonNetlist(TypedDict):
+class JsonNetlist(BaseModel):
   nets: list[JsonNet]
   nodes: JsonGraph
   graphUiData: Any  # ignored
@@ -49,7 +50,7 @@ class JsonNetlist(TypedDict):
 
 if __name__ == '__main__':
   with open("BasicBlinky.json") as f:
-    # netlist: JsonNetlist = json.load(f)
-    netlist = JsonNetlist(**json.load(f))
+    netlist = JsonNetlist.model_validate_json(f.read())
+
     print(netlist)
     print(netlist.nodes)
