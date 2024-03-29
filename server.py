9de972cc19da0a078acf96b9e9075b03aef85066
Richard Lin <elpato25@gmail.com>
2024-03-29 10:20:08 -0700

    proper cors

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from typing import Optional, List
 
 from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
 from pydantic import BaseModel, ValidationError
 
 from netlist_compiler import JsonNetlist, compile_netlist
@@ -12,6 +13,7 @@ class NetlistCompilerResponse(BaseModel):
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/version", methods=['GET'])
@@ -20,33 +22,19 @@ def version():
 
 @app.route("/compile", methods=['POST', 'OPTIONS'])
 def compile():
-  if request.method == "OPTIONS": # CORS preflight
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "*")
-    response.headers.add("Access-Control-Allow-Methods", "*")
-    return response
-
   try:
     json_netlist = JsonNetlist.model_validate_json(request.get_data())
   except ValidationError as e:
-    response = jsonify(NetlistCompilerResponse(kicadNetlist=None, errors=["invalid input format"]).model_dump()), 400
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return jsonify(NetlistCompilerResponse(kicadNetlist=None, errors=["invalid input format"]).model_dump()), 400
 
   try:
     kicad_netlist, model_errors = compile_netlist(json_netlist)
   except Exception as e:
-    response = jsonify(NetlistCompilerResponse(kicadNetlist=None, errors=[str(e)]).model_dump()), 400
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return jsonify(NetlistCompilerResponse(kicadNetlist=None, errors=[str(e)]).model_dump()), 400
 
   response = jsonify(NetlistCompilerResponse(
     kicadNetlist=kicad_netlist,
     errors=model_errors
   ).model_dump())
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', '*')
-  response.headers.add('Access-Control-Allow-Methods', '*')
 
   return response
