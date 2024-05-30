54cec388e9fd3e40db49f82ae6b837efc3f21a68
Richard Lin <elpato25@gmail.com>
2024-05-30 09:30:58 -0700

    Update server.py

--- server.py
+++ server.py
@@ -20,16 +20,25 @@ def compile():
     json_netlist = JsonNetlist.model_validate_json(request.get_data())
     result = compile_netlist(json_netlist)
   except JsonNetlistValidationError as e:
-    return jsonify(CompilerResult(errors=[
-      CompilerError(path=[e.path], kind="invalid input", details=e.desc)
-    ]).model_dump()), 400
+    return jsonify(CompilerResult(
+      edgHdl="",
+      errors=[
+        CompilerError(path=[e.path], kind="invalid input", details=e.desc)
+      ]
+    ).model_dump()), 400
   except ValidationError as e:
-    return jsonify(CompilerResult(errors=[
-      CompilerError(path=[], kind="invalid input", details="format error")
-    ]).model_dump()), 400
+    return jsonify(CompilerResult(
+      edgHdl="",
+      errors=[
+        CompilerError(path=[], kind="invalid input", details="format error")
+      ]
+    ).model_dump()), 400
   except Exception as e:
-    return jsonify(CompilerResult(errors=[
-      CompilerError(path=[], kind=f"internal error", details=repr(e))
-    ]).model_dump()), 400
+    return jsonify(CompilerResult(
+      edgHdl="",
+      errors=[
+        CompilerError(path=[], kind=f"internal error", details=repr(e))
+      ]
+    ).model_dump()), 400
 
   return jsonify(result.model_dump())
