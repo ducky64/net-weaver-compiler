4fd5b6fd8daa541d5c9e39f07f0d00d2cf29d814
Richard Lin <elpato25@gmail.com>
2024-09-03 13:33:48 -0700

    ConnectorLed skeleton

---
+++ test_connectorled.py
@@ -0,0 +1,31 @@
+import unittest
+import os.path
+
+from netlist_compiler import tohdl_netlist, compile_netlist, JsonNetlist
+
+
+EXPECTED_HDL = """\
+class MyModule(SimpleBoardTop):
+  def __init__(self):
+    super().__init__()
+
+    self.PinHeader254Vertical = self.Block(PinHeader254Vertical(length=3))
+    self.IndicatorLed = self.Block(IndicatorLed())
+
+    self.connect(self.IndicatorLed.signal, self.PinHeader254Vertical.port_0)
+    self.connect(self.IndicatorLed.gnd, self.PinHeader254Vertical.port_1)
+"""
+
+
+class ConnectorLedTestCase(unittest.TestCase):
+  def test_compile(self):
+    # the server messes with cwd so we need to use the absolute path
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "tests/ConnectorLed.json")) as f:
+      netlist_data = f.read()
+      netlist = JsonNetlist.model_validate_json(netlist_data)
+
+    hdl = tohdl_netlist(netlist)
+    self.assertEqual(hdl, EXPECTED_HDL)
+
+    result = compile_netlist(netlist)  # just check it doesn't error out
+    self.assertEqual(result.errors, [])
