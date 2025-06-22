2d0d5dc92bb880ad8d170686a0fd97f3b8eabde7
Richard Lin <elpato25@gmail.com>
2025-06-22 00:00:20 -0700

    Add charlieplexing w/ RGB test case

---
+++ test_charliematrixrgb.py
@@ -0,0 +1,34 @@
+import unittest
+import os.path
+
+from netlist_compiler import tohdl_netlist, compile_netlist, JsonNetlist
+
+
+EXPECTED_HDL = """\
+class MyModule(SimpleBoardTop):
+  def __init__(self):
+    super().__init__()
+
+    self.Xiao_Esp32c3 = self.Block(Xiao_Esp32c3())
+    self.IndicatorSinkRgbLed = self.Block(IndicatorSinkRgbLed())
+    self.CharlieplexedLedMatrix = self.Block(CharlieplexedLedMatrix(nrows=2, ncols=3))
+
+    self.connect(self.Xiao_Esp32c3.gnd)
+    self.connect(self.IndicatorSinkRgbLed.signals, self.Xiao_Esp32c3.gpio.request_vector('gpio_13'))
+    self.connect(self.IndicatorSinkRgbLed.pwr, self.Xiao_Esp32c3.pwr_out)
+    self.connect(self.CharlieplexedLedMatrix.ios, self.Xiao_Esp32c3.gpio.request_vector('gpio_14'))
+"""
+
+
+class CharlieMatrixTestCase(unittest.TestCase):
+  def test_compile(self):
+    # the server messes with cwd so we need to use the absolute path
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "tests/CharlieMatrixWithRgb.json")) as f:
+      netlist_data = f.read()
+      netlist = JsonNetlist.model_validate_json(netlist_data)
+
+    hdl = tohdl_netlist(netlist)
+    self.assertEqual(hdl, EXPECTED_HDL)
+
+    result = compile_netlist(netlist)  # just check it doesn't error out
+    self.assertEqual(result.errors, [])
