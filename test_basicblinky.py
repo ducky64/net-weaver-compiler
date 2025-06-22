b96d907012434e4e4c68d13b1bee2436875a551b
Richard Lin <elpato25@gmail.com>
2025-06-21 21:02:29 -0700

    Update latest libraries

--- test_basicblinky.py
+++ test_basicblinky.py
@@ -29,18 +29,19 @@ EXPECTED_NETLIST = [
 EXPECTED_KICAD_NETLIST = """\
 (export (version D)
 (components
-(comp (ref "Xiao_Esp32c3")
+(comp (ref "U1")
   (value "XIAO ESP32C3")
-  (footprint "Seeed Studio XIAO Series Library:XIAO-Generic-Hybrid-14P-2.54-21X17.8MM")
+  (footprint "Seeed Studio XIAO Series Library:XIAO-ESP32C3-SMD")
   (property (name "Sheetname") (value ""))
   (property (name "Sheetfile") (value ""))
   (property (name "edg_path") (value "Xiao_Esp32c3"))
   (property (name "edg_short_path") (value "Xiao_Esp32c3"))
   (property (name "edg_refdes") (value "U1"))
   (property (name "edg_part") (value "XIAO ESP32C3"))
+  (property (name "edg_value") (value "XIAO ESP32C3"))
   (sheetpath (names "/") (tstamps "/"))
   (tstamps "1c780414"))
-(comp (ref "IndicatorLed.package")
+(comp (ref "D1")
   (value "Red 615~630nm 1.9~2.2V 0603 Light Emitting Diodes (LED) RoHS")
   (footprint "LED_SMD:LED_0603_1608Metric")
   (property (name "Sheetname") (value "IndicatorLed"))
@@ -49,9 +50,10 @@ EXPECTED_KICAD_NETLIST = """\
   (property (name "edg_short_path") (value "IndicatorLed.package"))
   (property (name "edg_refdes") (value "D1"))
   (property (name "edg_part") (value "KT-0603R (Hubei KENTO Elec)"))
+  (property (name "edg_value") (value "Red 615~630nm 1.9~2.2V 0603 Light Emitting Diodes (LED) RoHS"))
   (sheetpath (names "/IndicatorLed/") (tstamps "/1e4704b3/"))
   (tstamps "0b4e02cd"))
-(comp (ref "IndicatorLed.res")
+(comp (ref "R1")
   (value "±1% 1/10W Thick Film Resistors 75V ±100ppm/℃ -55℃~+155℃ 1kΩ 0603 Chip Resistor - Surface Mount ROHS")
   (footprint "Resistor_SMD:R_0603_1608Metric")
   (property (name "Sheetname") (value "IndicatorLed"))
@@ -60,31 +62,30 @@ EXPECTED_KICAD_NETLIST = """\
   (property (name "edg_short_path") (value "IndicatorLed.res"))
   (property (name "edg_refdes") (value "R1"))
   (property (name "edg_part") (value "0603WAF1001T5E (UNI-ROYAL(Uniroyal Elec))"))
+  (property (name "edg_value") (value "±1% 1/10W Thick Film Resistors 75V ±100ppm/℃ -55℃~+155℃ 1kΩ 0603 Chip Resistor - Surface Mount ROHS"))
   (sheetpath (names "/IndicatorLed/") (tstamps "/1e4704b3/"))
   (tstamps "0296014b")))
 (nets
 (net (code 1) (name "IndicatorLed.signal")
-  (node (ref Xiao_Esp32c3) (pin 2))
-  (node (ref IndicatorLed.package) (pin 2)))
+  (node (ref U1) (pin 2))
+  (node (ref D1) (pin 2)))
 (net (code 2) (name "IndicatorLed.gnd")
-  (node (ref Xiao_Esp32c3) (pin 13))
-  (node (ref IndicatorLed.res) (pin 2)))
+  (node (ref U1) (pin 13))
+  (node (ref R1) (pin 2)))
 (net (code 3) (name "Xiao_Esp32c3.pwr_out")
-  (node (ref Xiao_Esp32c3) (pin 12)))
+  (node (ref U1) (pin 12)))
 (net (code 4) (name "Xiao_Esp32c3.vusb_out")
-  (node (ref Xiao_Esp32c3) (pin 14)))
+  (node (ref U1) (pin 14)))
 (net (code 5) (name "IndicatorLed.res.a")
-  (node (ref IndicatorLed.res) (pin 1))
-  (node (ref IndicatorLed.package) (pin 1))))
+  (node (ref R1) (pin 1))
+  (node (ref D1) (pin 1))))
 )"""
 
 EXPECTED_SVGPCB_INSTANTIATIONS = [
-  "const Xiao_Esp32c3 = board.add(XIAO_Generic_Hybrid_14P_2_54_21X17_8MM, {\n  translate: pt(0.350, 0.000), rotate: 0,\n  id: 'Xiao_Esp32c3'\n})",
-  "const IndicatorLed_package = board.add(LED_0603_1608Metric, {\n  translate: pt(0.859, 0.000), rotate: 0,\n  id: 'IndicatorLed_package'\n})",
-  "const IndicatorLed_res = board.add(R_0603_1608Metric, {\n  translate: pt(1.076, 0.000), rotate: 0,\n  id: 'IndicatorLed_res'\n})"
+  "const Xiao_Esp32c3 = board.add(XIAO_ESP32C3_SMD, {\n  translate: pt(0.348, 0.000), rotate: 0,\n  id: 'Xiao_Esp32c3'\n})",
+  "const IndicatorLed_package = board.add(LED_0603_1608Metric, {\n  translate: pt(0.858, 0.000), rotate: 0,\n  id: 'IndicatorLed_package'\n})",
+  "const IndicatorLed_res = board.add(R_0603_1608Metric, {\n  translate: pt(1.075, 0.000), rotate: 0,\n  id: 'IndicatorLed_res'\n})"
 ]
-
-
 
 
 class BasicBlinkyTestCase(unittest.TestCase):
